d7e74225ba3fb08762e4961cd22954866b45de24
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2024-12-26 16:01:30 -0500

    fixed sticky honey residue face check

--- common/src/main/java/com/telepathicgrunt/the_bumblezone/blocks/StickyHoneyResidue.java
+++ common/src/main/java/com/telepathicgrunt/the_bumblezone/blocks/StickyHoneyResidue.java
@@ -222,7 +222,7 @@ public class StickyHoneyResidue extends Block {
         for (Direction direction : Direction.values()) {
             BooleanProperty booleanproperty = FACING_TO_PROPERTY_MAP.get(direction);
             if (blockstate.getValue(booleanproperty)) {
-                if (GeneralUtils.isFaceFullFast(world, pos.relative(direction), direction)) {
+                if (GeneralUtils.isFaceFullFast(world, pos.relative(direction), direction.getOpposite())) {
                     return true;
                 }
             }
@@ -260,7 +260,7 @@ public class StickyHoneyResidue extends Block {
         for (Direction direction : Direction.values()) {
             BooleanProperty booleanproperty = FACING_TO_PROPERTY_MAP.get(direction);
             if (blockstate.getValue(booleanproperty)) {
-                boolean flag = GeneralUtils.isFaceFullFast(blockReader, pos.relative(direction), direction);
+                boolean flag = GeneralUtils.isFaceFullFast(blockReader, pos.relative(direction), direction.getOpposite());
                 blockstate = blockstate.setValue(booleanproperty, flag);
             }
         }
